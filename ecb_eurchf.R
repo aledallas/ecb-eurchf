9d9f068dc4c6ec0d2748951b4c2ceb6f97560386
Alessandro <71699809+aledallas@users.noreply.github.com>
2021-04-20 17:02:59 +0200

    Create ecb_eurchf.R

---
+++ ecb_eurchf.R
@@ -0,0 +1,25 @@
+# Retrieve EUr/CHF currency exchange data from ECB
+library(tidyverse)
+library(xml2)
+library(writexl)
+
+url <- "https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/chf.xml"
+
+pg <- read_html(url)
+
+# extract obesrvations
+obs_raw <- pg %>% 
+  xml_find_all(".//obs") %>% # more than three times faster than going through xml_child
+  xml_attrs()
+
+# do some cleaning
+obs <- obs_raw %>% 
+  enframe() %>% 
+  unnest_wider(value) %>% 
+  select(-name) %>% 
+  mutate(time_period = as.Date(time_period),
+         obs_value = as.numeric(obs_value)) %>% 
+  arrange(desc(time_period))
+
+# write output
+writexl::write_xlsx(obs, "data/ecb_eurchf.xlsx")
